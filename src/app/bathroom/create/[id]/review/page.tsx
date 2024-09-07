bb84575f31c5782c1028989f072238e5348f8d06
Andresr35 <andresjruiz@cpp.edu>
2024-09-07 15:15:55 -0700

    add states to all formdata

--- src/app/bathroom/create/[id]/review/page.tsx
+++ src/app/bathroom/create/[id]/review/page.tsx
@@ -9,46 +9,82 @@ import Review from "@/components/Review";
 import TP from "@/components/Tp";
 import Women from "@/components/Women";
 import Image from "next/image";
+import { useParams } from "next/navigation";
+import { useState } from "react";
 
-const Comment = () => {
+const Comment = ({
+  comment,
+  setComment,
+}: {
+  comment: string;
+  setComment: (val: string) => void;
+}) => {
   return (
     <textarea
       className="flex p-3 min-h-[60px] min-w-[250px] rounded-md"
+      placeholder="Type your message..."
+      value={comment}
+      onChange={(e) => setComment(e.target.value)}
       style={{
         color: "#A17063",
         border: "1px solid var(--Color-Brand-accent, #BB8F83)",
         borderRadius: "5px",
       }}
-    >
-      Type your message...
-    </textarea>
+    />
   );
 };
 
-const page = () => {
+const Page = () => {
+  const { id } = useParams();
+  const [reviewScore, setReviewScore] = useState(0);
+  const [isAccessible, setIsAccessible] = useState(false);
+  const [hasFemale, setHasFemale] = useState(false);
+  const [hasMale, setHasMale] = useState(false);
+  const [hasTP, setHasTP] = useState(false);
+  const [isPrivate, setIsPrivate] = useState(false);
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = () => {
+    console.log("bryan will submit this data");
+  };
+  const handleExit = () => {
+    console.log("bryan will exit this page");
+  };
+
   return (
     <div>
       <div id="brick" />
       <div
+        onClick={handleExit}
+        style={{ position: "absolute", top: "10px", left: "10px" }}
+      >
+        <Image width={24} height={24} src="/icons/x.png" alt="urinal" />
+      </div>
+      <div
         className="flex min-w-[321px] py-5 px-4 flex-col items-start gap-4 rounded-xl "
         style={{ background: "#FAF9F9", margin: "71px 36px" }}
       >
-        <Review />
+        <Review reviewScore={reviewScore} setReviewScore={setReviewScore} />
         <div>Bar</div>
         <div className="flex gap-4">
-          <Accessibility />
-          <Women />
-          <Men />
-          <TP />
-          <Private />
+          <Accessibility
+            filled={isAccessible}
+            onclick={() => setIsAccessible(!isAccessible)}
+          />
+          <Women filled={hasFemale} onclick={() => setHasFemale(!hasFemale)} />
+          <Men filled={hasMale} onclick={() => setHasMale(!hasMale)} />
+          <TP filled={hasTP} onclick={() => setHasTP(!hasTP)} />
+          <Private
+            filled={isPrivate}
+            onclick={() => setIsPrivate(!isPrivate)}
+          />
         </div>
-        <Amenities>
-          <p style={{ color: "#A17063" }}>Girly pop stuff</p>
-        </Amenities>
-        <Comment />
+        {/* <Amenities ameneties ={amenities} setAmenities={setAmenities} /> */}
+
+        <Comment comment={comment} setComment={setComment} />
         <Button
           variant={BUTTON_VARIANTS.SOLID}
-          onclick={() => console.log("clicked")}
+          onclick={handleSubmit}
           className="py-2 px-4"
         >
           <p>Enter</p>
@@ -68,4 +104,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
--- src/components/Amenities.tsx
+++ src/components/Amenities.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 interface AmenitiesProps {
   children: React.ReactNode;
+  onclick?: () => void;
 }
-const Amenities = ({ children }: AmenitiesProps) => {
+const Amenities = ({ children, onclick }: AmenitiesProps) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  // Handler to toggle the checked status
+  const handleChange = (event: any) => {
+    setIsChecked(event.target.checked);
+  };
   return (
     <div className="flex gap-2 items-center">
       <input
+        checked={isChecked}
+        onChange={handleChange}
         type="checkbox"
         className="w-4 h-4"
         style={{ border: "1px solid var(--Color-Brand-Button, #A17063)" }}
